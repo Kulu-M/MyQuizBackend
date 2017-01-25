2199bc0c84c5e5a7809d0294ccaacc8a1ace2221
Kulu-M <lloyd.niebel@googlemail.com>
2017-01-25 23:39:55 +0100

    User auth started - needs update on Model
    Passwords not in model

---
+++ MyQuizBackend/src/MYVote/Classes/DeviceAuthentification.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyQuizBackend
+{
+    public class DeviceAuthentification
+    {
+        public bool authenticateAdminDevice(int inputPassword)
+        {
+            return inputPassword == Constants.adminPassword;
+        }
+
+        public bool authenticateClientDevice(int deviceID)
+        {
+            //what we want to check here?
+            return false;
+        }
+
+        
+    }
+}
