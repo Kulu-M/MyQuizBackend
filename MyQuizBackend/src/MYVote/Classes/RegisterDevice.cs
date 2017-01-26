aad40b6ca22e0bc0f2fe29b2c44227e390f7d93d
Lloyd <lloyd.niebel@googlemail.com>
2017-01-26 14:48:05 +0100

    Moved to Classes

---
+++ MyQuizBackend/src/MYVote/Classes/RegisterDevice.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MYVote.Models
+{
+    public class RegistrationDevice
+    {
+        public string token;
+        public int deviceID;
+        public string password;
+
+        public RegistrationDevice(string token, int deviceID, string password)
+        {
+            this.token = token;
+            this.deviceID = deviceID;
+            this.password = password;
+        }
+    }
+}
