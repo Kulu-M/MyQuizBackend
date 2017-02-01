f4b76f5e7a4cd98211f62a9a02be066af77d20d9
Kulu-M <lloyd.niebel@googlemail.com>
2017-02-01 22:12:20 +0100

    Added route to quickly get exceptionlog w/o remote

---
+++ MyQuizBackend/src/MYVote/Controllers/LogFileController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MYVote;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MyQuizBackend.Controllers
+{
+    [Route("api/[controller]")]
+    public class LogFileController : Controller
+    {
+        // GET: api/logfile
+        [HttpGet]
+        public FileStreamResult GetLogFile()
+        {
+            Response.Headers.Add("content-disposition", "attachment; filename=ExceptionLogFile.txt");
+            var pathToLogFile = Path.Combine(Startup._iHostingEnv.ContentRootPath, @"Logs\ExceptionLogFile.txt");
+
+            return File(new FileStream(pathToLogFile, FileMode.Open),
+                "application/octet-stream");
+        }
+    }
+}
